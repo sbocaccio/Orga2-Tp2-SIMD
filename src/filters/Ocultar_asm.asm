ff448efdd701b625bf494ef86ab70f6ab139b15c
Gastón Máspero <gaston.maspero@gmail.com>
2020-06-04 17:07:23 -0700

    fix in ocultar filter

--- src/filters/Ocultar_asm.asm
+++ src/filters/Ocultar_asm.asm
@@ -1,7 +1,8 @@
+ALIGN 16
 MascaraConseguirRojo:       db 0x02,0xFF,0xFF,0xFF,0x06,0xFF,0xFF,0xFF,0x0A,0xFF,0xFF,0xFF,0x0E,0xFF,0xFF,0xFF
 MascaraConseguirGreen:      db 0x01,0xFF,0xFF,0xFF,0x05,0xFF,0xFF,0xFF,0x09,0xFF,0xFF,0xFF,0x0D,0xFF,0xFF,0xFF
 MascaraConseguirBlue:       db 0x00,0xFF,0xFF,0xFF,0x04,0xFF,0xFF,0xFF,0x08,0xFF,0xFF,0xFF,0x0C,0xFF,0xFF,0xFF
-MascaraConservarPrimerbit:  db 0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00
+MascaraConservarPrimerbit:  db 0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x01,0x00,0x00,0x00    
 MascaraConservarPrimer2bit: db 0x03,0x03,0x03,0x00,0x03,0x03,0x03,0x00,0x03,0x03,0x03,0x00,0x03,0x03,0x03,0x00
 MascaraSacarUltimos2Bits:   db 0xFC,0xFC,0xFC,0xFF,0xFC,0xFC,0xFC,0xFF,0xFC,0xFC,0xFC,0xFF,0xFC,0xFC,0xFC,0xFF
 MascaraPatrones:            db 0xFF,0x00,0x00,0xFF,0x00,0xFF,0x00,0xFF,0x00,0x00,0xFF,0xFF,0xFF,0x00,0x00,0xFF
@@ -9,7 +10,6 @@ MascaraGrises:              db 0x00,0x00,0x00,0xFF,0x04,0x04,0x04,0xFF,0x08,0x08
 PonerAlpha:                 db 0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF
 MascaraInvertir:            db 0x0C,0x0D,0x0E,0x0F,0x08,0x09,0x0A,0x0B,0x04,0x05,0x06,0x07,0x00,0x01,0x02,0x03
 MascaraFF:                  db 0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,0x00              
-;MascaraInvertirOrden:       db 
 global Ocultar_asm
 Ocultar_asm:
 %define REG_SRC rdi
@@ -25,26 +25,25 @@ Ocultar_asm:
 push rbp
 mov rbp,rsp
 push rbx
-push r12
+
 push r13
 push r14
 push r15
 push r10
 ;-------
 xor rbx,rbx
-xor r12,r12 ; CONTADOR FILA
 xor r13,r13 ; CONTADOR COLUMNA
 xor r14,r14 ; Le doy un uso super especifico al final, equivale a CANTIDAD DE COLUMNAS -5
-xor r15,r15; (Lo uso en un momento para un offset)
+xor r15,r15; 
 xor r10,r10 ; puntero a final de la foto la uso para Espejo
 xor rax,rax
 ;-------
-movdqu xmm3,[MascaraConseguirRojo]
-movdqu xmm2,[MascaraConseguirGreen]            
-movdqu xmm1,[MascaraConseguirBlue]
-movdqu xmm0,[MascaraConservarPrimerbit]
-movdqu xmm15,[MascaraSacarUltimos2Bits]
-movdqu xmm13,[MascaraInvertir]
+movdqa xmm3,[MascaraConseguirRojo]
+movdqa xmm2,[MascaraConseguirGreen]            
+movdqa xmm1,[MascaraConseguirBlue]
+movdqa xmm0,[MascaraConservarPrimerbit]
+movdqa xmm15,[MascaraSacarUltimos2Bits]
+movdqa xmm13,[MascaraInvertir]
 mov r14d, ecx
 sub r14d,5        ;-> Debo continuar por cada fila hasta r14d
 
@@ -53,10 +52,13 @@ imul eax, r8d  ;----> Final de la foto a SRC, va disminuyendo a medida que voy p
 sub eax,16
 mov r10d,eax
 
+mov eax, r8d
+imul eax,ecx
+mov r15,rax 
+xor rax,rax
 
 ciclo:
 
-xor rax,rax
 pxor xmm5,xmm5 ; Aca guardo la informacion de los pixeles,original
 pxor xmm6,xmm6 ; Aca guardo en el primer byte por dword el color de cada pixel
 pxor xmm7,xmm7 ; RojoBits 
@@ -66,15 +68,6 @@ pxor xmm9,xmm9 ; AzulBits
 												 	
 ;Consigo el valor de los 4 pixeles              
 												 	
-; Offset de la fila                            					
-												
-	mov eax,r9d   ; TAMANO FILA y dato 
-	imul eax, r12d ; CONTADOR FILA 
-	  
- ; Offset del dato
- 	mov r15d,r13d ; contador columna    
- 	shl r15d,2
- 	add eax,r15d
 
 	;copiamos el dato
 	movdqu xmm4, [REG_SRC2+rax]
@@ -163,7 +156,7 @@ por xmm8,xmm9
 ;----------------------- Ya tengo los colores de blanco y negro              
 BitsColores:
 ;Pixeles Espejo
-movdqu xmm11 , [REG_SRC + r10] ;XMM2 tiene el valor de SRC ESPEJO                  
+movdqu xmm11 , [REG_SRC + r10] ; tiene el valor de SRC ESPEJO                  
 pshufb xmm11, xmm13 ; Los invierto   00011011
 psrld xmm11,2          ; Los corro dos bits para sacar los 2 y 3 
 pand xmm11, [MascaraConservarPrimer2bit] ; Me quedo con los bits que quiero
@@ -176,26 +169,20 @@ Filtro:
 pand xmm14, xmm15 ; Limpio los 2 ultimos bits y copio
 por xmm14,xmm8                              
 movdqu [REG_ACOPIAR+rax],xmm14
-xor rax,rax
+
 ;Ahora hay que ver si tenemos que seguir ciclando o solo aumentar las variables con las que nos movemos
 
 ;Aumentar i / j 
  
-	cmp r13d, r14d ; -> CONTADOR DE COLUMNAS MENOR A EL NUMERO DE COLUMNAS - 5  ()
-	jl incCol  ; 
-	incrementarFila:
+	; 
+	add rax,16
 	sub r10,16
-	inc r12d
-	cmp r12d,r8d
-	je termine
-	xor r13,r13 
-	jmp ciclo
+	add r13,4
 
-	incCol:
-	sub r10,16
-	add r13d,4
+	cmp r13, r15 ;
+	je termine  
+	
 	jmp ciclo
-
 
 	termine:
 	mov rax,rdx
@@ -210,7 +197,6 @@ pop r10
 pop r15                                          
 pop r14
 pop r13
-pop r12
 pop rbx   
 pop rbp
 ret
